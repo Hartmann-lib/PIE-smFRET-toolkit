a79cb6eb82196c88fe5cff14b3115aeec4ba3b62
Andreas Hartmann <58071484+Hartmann-lib@users.noreply.github.com>
2025-07-30 23:24:15 +0200

    Update S2_Analyse_HT3_with_Bin_Time_2Channels.py

--- S2_Analyse_HT3_with_Bin_Time_2Channels.py
+++ S2_Analyse_HT3_with_Bin_Time_2Channels.py
@@ -11,7 +11,7 @@ import json
 #########################################################################
 DATA_FOLDER = "20240725HFRuler"
 
-SETTINGS_FILE = "Settings_20250730_094959"
+SETTINGS_FILE = "Settings_20250728_202520"
 
 BIN_T = 1 # (ms) bin time
 THRE_B = 30 # (kHz) lower intensity threshold for bin selection see ALGORITHM
